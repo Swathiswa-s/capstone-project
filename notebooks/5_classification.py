ce52d8a2172cd2ead6268637d8a90d8be1097280
Swathiswa-s <146329379+Swathiswa-s@users.noreply.github.com>
2026-04-01 12:06:09 +0530

    Update 5_classification.py

--- notebooks/5_classification.py
+++ notebooks/5_classification.py
@@ -1,1 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pickle
 
+df = pd.read_csv("../data/cleaned_jobs.csv")
+
+X = df[['Lag1','Lag2','Lag3','Lag4','Lag5']]
+y = df['Direction']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+accuracy = model.score(X_test, y_test)
+print("Accuracy:", accuracy)
+
+# Save model
+pickle.dump(model, open("../models/model.pkl", "wb"))
