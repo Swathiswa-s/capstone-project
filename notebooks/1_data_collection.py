446128d718a719fc124e6a92ceb7256df6644f5f
Swathiswa-s <146329379+Swathiswa-s@users.noreply.github.com>
2026-04-01 12:04:44 +0530

    Update 1_data_collection.py

--- notebooks/1_data_collection.py
+++ notebooks/1_data_collection.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+# Load dataset
+url = "https://raw.githubusercontent.com/selva86/datasets/master/Smarket.csv"
+df = pd.read_csv(url)
+
+print(df.head())
+
+# Save raw data
+df.to_csv("../data/raw_jobs.csv", index=False)
