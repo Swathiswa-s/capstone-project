75e873423925fd657795c05bba0d21669013817d
Swathiswa-s <146329379+Swathiswa-s@users.noreply.github.com>
2026-04-01 12:05:24 +0530

    Update 3_analysis.py

--- notebooks/3_analysis.py
+++ notebooks/3_analysis.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("../data/cleaned_jobs.csv")
+
+# Summary statistics
+print(df.describe())
+
+# Plot volume over years
+plt.plot(df['Year'], df['Volume'])
+plt.title("Market Volume Over Years")
+plt.xlabel("Year")
+plt.ylabel("Volume")
+plt.show()
+
+# Direction count
+df['Direction'].value_counts().plot(kind='bar')
+plt.title("Market Direction Count")
+plt.show()
