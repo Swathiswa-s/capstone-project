b488a4b5eab165f52f79b6313a9de882d1912d11
Swathiswa-s <146329379+Swathiswa-s@users.noreply.github.com>
2026-04-01 12:05:49 +0530

    Update 4_clustering.py

--- notebooks/4_clustering.py
+++ notebooks/4_clustering.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from sklearn.cluster import KMeans
 
+df = pd.read_csv("../data/cleaned_jobs.csv")
+
+X = df[['Lag1', 'Lag2']]
+
+kmeans = KMeans(n_clusters=3, random_state=42)
+df['Cluster'] = kmeans.fit_predict(X)
+
+print(df.head())
