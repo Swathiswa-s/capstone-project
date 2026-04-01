553d6b5674ea7cff9a9590954f030a4b3f4e1c34
Swathiswa-s <146329379+Swathiswa-s@users.noreply.github.com>
2026-04-01 12:05:03 +0530

    Update 2_data_cleaning.py

--- notebooks/2_data_cleaning.py
+++ notebooks/2_data_cleaning.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+df = pd.read_csv("../data/raw_jobs.csv")
+
+# Check missing values
+print(df.isnull().sum())
+
+# Remove duplicates
+df = df.drop_duplicates()
+
+# Convert target column
+df['Direction'] = df['Direction'].map({'Up': 1, 'Down': 0})
+
+print(df.head())
+
+# Save cleaned data
+df.to_csv("../data/cleaned_jobs.csv", index=False)
